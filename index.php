becd757c7a7bc799a94fab7cefcb477103a3c96c
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2021-11-19 13:37:40 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="text">
+    <h1>Portfolio</h1>
+  </div>
+  <div class="main">
+    <div class="information">
+      <ul >
+        <li>Name: Meerim</li>
+        <li>Surname: Momunbaeva</li>
+        <li>Birth-date: 04.10.2004</li>
+
+      </ul>
+    </div>
+    <div class="photo">
+      <img src="avatar.png" alt="" srcset="" class="img
+      ">
+    </div>
+  </div>
+</body>
+</html>
